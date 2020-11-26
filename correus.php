3c5901a0cef23496191670ebb021e7bd766ffa07
oscargonzalezgarcia <45206878+oscargonzalezgarcia@users.noreply.github.com>
2020-11-26 17:48:21 +0100

    Fitxers PHP Credencials

---
+++ correus.php
@@ -0,0 +1,7 @@
+<?php
+    define('CORREUS',[
+        '0'       => 'admin@educem.com',
+        '1'       => 'donald@educem.com',
+        '2'       => 'gilete@educem.com',
+        '3'       => 'gon@educem.com',
+    ]);
---
+++ passwords.php
@@ -0,0 +1,7 @@
+<?php
+    define('PASSWORDS',[
+            '0'       => 'iloveu',
+            '1'       => 'm4k3Am3r1caGr3atAg41n!',
+            '2'       => 'ErF4ryS1empr3',
+            '3'       => 'Fatality!',
+        ]);
